761463ec24f2dfeb404441a4ea4ceb397047c393
Ysadoriel <43242736+Ysadoriel@users.noreply.github.com>
2018-09-27 19:56:29 +0300

    Added Skeleton of Store API

---
+++ StoreAPI/Models/OrderProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreAPI.Models
+{
+    public class OrderProduct
+    {
+        public ICollection<Product> Products { get; set; }
+        public ICollection<Order> Orders { get; set; }
+        public string Adress { get; set; }
+        public int ProductQuantity{get; set;}
+
+    }
+}
